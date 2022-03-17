7c4afb35bed0d1306f2c841f7b05c1a4bdcb95e0
nub69 <shabik.ashab@gmail.com>
2022-03-17 11:17:06 +0600

    initial

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+import TableList from '../Components/TableList/TableList'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <TableList />
+    </div>
+  )
+}
+
+export default Home
